e58fb0b46c67669586e90e3843456d03270888fd
Miki <110220534+IAmMichaelP@users.noreply.github.com>
2025-05-22 19:22:52 +0800

    added cron server back to the main node service

---
+++ Service/node-service/cron.js
@@ -0,0 +1,22 @@
+// cron-server/index.js
+require('dotenv').config();
+const mongoose = require('mongoose');
+const cron = require('node-cron');
+const { fetchAndStoreArticles } = require('./controllers/newsController');
+const cors = require('cors');
+
+// Middleware
+app.use(cors());
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB from CRON server');
+
+  // Initial run
+  fetchAndStoreArticles();
+
+  // Schedule to run every hour
+  cron.schedule('0 * * * *', fetchAndStoreArticles);
+});
