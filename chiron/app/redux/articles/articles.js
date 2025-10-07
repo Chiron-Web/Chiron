a9f06576941786d5e7b14954acbdedef78451eb5
Miki <110220534+madpurson@users.noreply.github.com>
2025-10-08 00:59:51 +0800

    adding pagesize

--- chiron/app/redux/articles/articles.js
+++ chiron/app/redux/articles/articles.js
@@ -1,25 +1,39 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { act } from "react";
 
 const initialArticlesState = {
   articles: [],
   hasMore: true,
+  isArticleLoading: true,
+  page: 1,
 };
 
 const articlesSlice = createSlice({
   name: "articles",
   initialState: initialArticlesState,
-  reducers: {},
+  reducers: {
+    addArticles: (state, action) => {
+      state.articles.push(...action.payload);
+    },
+    setHasMore: (state, action) => {
+      state.hasMore = action.payload;
+    },
+    setIsArticleLoading: (state, action) => {
+      state.isArticleLoading = action.payload;
+    }
+  },
   extraReducers: (builder) => {
     builder.addCase(fetchArticles.fulfilled, (state, action) => {
-      state.articles.push(...action.payload.articles);
-      state.hasMore = action.payload.hasMore;
+      addArticles(state, action.payload.articles);
+      setHasMore(state, action.payload.hasMore);
+      setIsArticleLoading(state, action.payload.isArticleLoading);
     });
   }
 });
 
 export const fetchArticles = createAsyncThunk(
     'articles/fetchArticles',
-    async ( pageNum, fetchUrl ) => {
+    async ( pageNum, fetchUrl, pageSize ) => {
         try {
           const response = await fetch(`${fetchUrl}?page=${pageNum}&pageSize=${pageSize}`);
           const data = await response.json();
@@ -27,10 +41,11 @@ export const fetchArticles = createAsyncThunk(
           if (data.success && data.articles.length > 0) {
             return {
               articles: data.articles,
-              hasMore: data.articles.length === pageSize
+              hasMore: data.articles.length === pageSize,
+              isArticleLoading: false
             }  
           } 
-          return { articles: [], hasMore: false };
+          return { articles: [], hasMore: false, isArticleLoading: false };
         } catch (err) {
           console.error('Error fetching articles:', err);
         }
