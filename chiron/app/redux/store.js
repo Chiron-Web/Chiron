f649256914c3da438cfdc188fe438b3e1e8c2789
Miki <pelegrinompatrick@gmail.com>
2025-10-16 23:44:56 +0800

    added the url reducer

--- chiron/app/redux/store.js
+++ chiron/app/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { articlesReducer } from "./articles/articles";
+import { urlReducer } from "./articles/url";
 
 export const store = configureStore({
   reducer: {
     articles: articlesReducer,
+    url: urlReducer,
   },
 });
