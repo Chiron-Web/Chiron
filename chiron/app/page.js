1369ccc97b882c4dad1fc6c28e8b877b8e7ff907
Miki <pelegrinompatrick@gmail.com>
2025-10-28 01:26:11 +0800

    Update page.js

--- chiron/app/page.js
+++ chiron/app/page.js
@@ -1,14 +1,10 @@
 'use client';
 import Homepage from "./components/homepage";
-import { store } from "./redux/store";
-import { Provider } from "react-redux";
 
 export default function Home() {
   return (
-    <Provider store={store}> 
       <main>
         <Homepage />
       </main>
-    </Provider>
   );
 }
